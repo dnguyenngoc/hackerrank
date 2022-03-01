af4b22e273db48062deb2aa71b4615005ca70060
Duy Nguyen <duynguyenngoc@hotmail.com>
2022-03-01 22:52:25 +0700

    add some problem solving

--- python/Find the Runner-Up Score!.py
+++ python/Find the Runner-Up Score!.py
@@ -1,12 +1,9 @@
 # https://www.hackerrank.com/challenges/find-second-maximum-number-in-a-list/problem?h_r=profile
-https://www.hackerrank.com/challenges/find-second-maximum-number-in-a-list/problem?h_r=profile
-if __name__ == '__main__':
-    n = int(input())
-    arr = map(int, input().split())
-    arr = list(arr)
-    max_ = max(arr)
-    start = arr[0]
-    for i in range(n):
-        if (arr[i] > start) and (arr[i] < max_):
-            start = arr[i]
-    print(start)
+
+# not check yet!
+i = int(input())
+lis = list(map(int,input().strip().split()))[:i]
+z = max(lis)
+while max(lis) == z:
+    lis.remove(max(lis))
+print(max(lis))
